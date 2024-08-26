03e59f56f0d2e1fa8a78216fafe3891a59e9b647
Jon Sterling <jon@jonmsterling.com>
2024-08-26 11:41:53 +0100

    clean up Import_graph

--- lib/frontend/Import_graph.ml
+++ lib/frontend/Import_graph.ml
@@ -1,3 +1,4 @@
+open Forester_prelude
 open Forester_core
 open Forester_compiler
 
@@ -14,13 +15,15 @@ let build (trees : Code.tree list) =
   let rec analyse_tree roots (tree : Code.tree) =
     let roots = Option.fold ~none:roots ~some:(fun x -> x :: roots) tree.addr in
     begin
-      tree.addr |> Option.iter @@ fun addr ->
+      let@ addr = Option.iter @~ tree.addr in
       Gph.add_vertex import_graph @@ Addr.user_addr addr
     end;
-    tree.code |> List.iter @@ fun node ->
+
+    let@ node = List.iter @~ tree.code in
     match Asai.Range.(node.value) with
     | Code.Import (_, dep) ->
-      roots |> List.iter @@ fun addr -> Gph.add_edge import_graph (Addr.user_addr dep) (Addr.user_addr addr)
+      let@ addr = List.iter @~ roots in
+      Gph.add_edge import_graph (Addr.user_addr dep) (Addr.user_addr addr)
     | Code.Subtree (addr, code) ->
       analyse_tree roots @@ Code.{tree with addr; code}
     | _ -> ()
