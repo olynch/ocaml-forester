31126d0384bf764a9bb520ec40a8f77662d4df66
Jon Sterling <jon@jonmsterling.com>
2024-08-26 11:41:48 +0100

    clean up Forest_scanner

--- lib/frontend/Forest_scanner.ml
+++ lib/frontend/Forest_scanner.ml
@@ -1,20 +1,23 @@
-module S = Algaeff.Sequencer.Make (struct type t = Eio.Fs.dir_ty Eio.Path.t end)
+open Forester_prelude
+module EP = Eio.Path
+module S = Algaeff.Sequencer.Make (struct type t = Eio.Fs.dir_ty EP.t end)
 
 let rec process_file fp =
-  if Eio.Path.is_directory fp then
+  if EP.is_directory fp then
     process_dir fp
   else
-    Eio.Path.split fp |> Option.iter @@ fun (_, basename) ->
+    let@ _, basename = Option.iter @~ EP.split fp in
     if Filename.extension basename = ".tree" && not @@ String.starts_with ~prefix:"." basename then
       S.yield fp
 
 and process_dir dir =
   try
-    Eio.Path.read_dir dir |> List.iter @@ fun fp ->
-    process_file Eio.Path.(dir / fp)
-  with Eio.Io (Eio.Fs.E (Permission_denied _), _) -> ()
+    let@ fp = List.iter @~ EP.read_dir dir in
+    process_file EP.(dir / fp)
+  with
+    Eio.Io (Eio.Fs.E (Permission_denied _), _) -> ()
 
 let scan_directories dirs =
-  S.run @@ fun () ->
-  dirs |> List.iter @@ fun fp ->
+  let@ () = S.run in
+  let@ fp = List.iter @~ dirs in
   process_dir fp
