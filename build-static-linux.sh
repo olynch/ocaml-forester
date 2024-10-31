90b33cc86534cd9d3edd3489a2b285f4cec36e52
Kento Okura <kentookura@gmail.com>
2024-10-31 18:49:00 +0100

    Try to build static binary in CI

--- build-static-linux.sh
+++ build-static-linux.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 mkdir -p docker-build
-docker build --output=docker-build --target=forester-built .
+docker buildx build . --output=forester
 
 # TODO: figure out how to automate getting the release version here
-tar -czf forester-4.2.0-prerelease-x86_64-unknown-linux-musl.tar.gz -C docker-build/bin forester
+tar -czf forester-5.0-dev-x86_64-unknown-linux-musl.tar.gz forester
