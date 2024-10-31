9558893f1d5f95ca6ca975d192e83c7448e1af65
Jon Sterling <jon@jonmsterling.com>
2024-10-31 12:27:13 +0000

    update license and copyright information

--- lib/language_server/Completion.ml
+++ lib/language_server/Completion.ml
@@ -1,3 +1,10 @@
+(*
+ * SPDX-FileCopyrightText: 2024 The Forester Project Contributors AND The RedPRL Development Team
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 WITH LLVM-exception
+ *
+ *)
+
 open Forester_compiler
 module L = Lsp.Types
 
