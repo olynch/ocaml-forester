fa5fab8997982bf082016d9ff24a301410d5f5e8
Kento Okura <kentookura@gmail.com>
2024-12-28 21:20:10 +0100

    Build statically using nix
    Original author: Owen Lynch

--- flake.nix
+++ flake.nix
@@ -10,14 +10,17 @@
     flake-utils.url = "github:numtide/flake-utils";
     opam-repository.url = "github:ocaml/opam-repository";
     opam-repository.flake = false;
-    opam-nix.url = "github:tweag/opam-nix";
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.opam-repository.follows = "opam-repository";
+    };
   };
   outputs =
     {
       flake-utils,
       opam-nix,
       nixpkgs,
-      opam-repository,
       ...
     }:
     let
@@ -27,30 +30,63 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        isGnu64 = system == "x86_64-linux";
+        pkgsStatic =
+          if isGnu64 then
+            import nixpkgs {
+              localSystem = nixpkgs.lib.systems.examples.gnu64;
+              crossSystem = nixpkgs.lib.systems.examples.musl64;
+            }
+          else
+            pkgs;
         on = opam-nix.lib.${system};
         devPackagesQuery = {
-          ocaml-base-compiler = "5.2.0";
           ocaml-lsp-server = "*";
           odig = "*";
           alcotest = "*";
         };
-        query = devPackagesQuery // { };
-        scope = on.buildOpamProject' { repos = [ "${opam-repository}" ]; } ./. query;
-        overlay = final: prev: {
-          ${package} = prev.${package}.overrideAttrs (_: {
-            doNixSupport = false;
-          });
+        query = devPackagesQuery // {
+          ocaml-system = "*";
         };
-        scope' = scope.overrideScope' overlay;
-        main = scope'.${package};
-        devPackages = builtins.attrValues (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+        mkScopes = pkgs: isStatic: rec {
+          scope = on.buildDuneProject { inherit pkgs; } package ./. query;
+          overlay = final: prev: {
+            ${package} = prev.${package}.overrideAttrs (
+              _:
+              {
+                doNixSupport = false;
+              }
+              // (
+                if isStatic then
+                  {
+                    buildPhase = ''dune build -p ${package} --profile static -j $NIX_BUILD_CORES'';
+                  }
+                else
+                  { }
+              )
+            );
+            ocamlgraph = prev.ocamlgraph.overrideAttrs (_: {
+              buildPhase = ''dune build -p ocamlgraph -j $NIX_BUILD_CORES'';
+            });
+            conf-gmp = prev.conf-gmp.overrideAttrs (_: {
+              nativeBuildInputs = [ pkgs.pkgsBuildHost.stdenv.cc ];
+            });
+          };
+          scope' = scope.overrideScope overlay;
+          main = scope'.${package};
+        };
+        scopes = mkScopes pkgs false;
+        scopesStatic = mkScopes pkgsStatic isGnu64;
+        devPackages = builtins.attrValues (
+          pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scopes.scope'
+        );
       in
       {
-        legacyPackages = scope';
-        packages.default = main;
+        legacyPackages = scopes.scope';
+        packages.default = scopesStatic.main;
         devShells.default = pkgs.mkShell {
           TOPIARY_LANGUAGE_DIR = "topiary";
-          inputsFrom = [ main ];
+          inputsFrom = [ scopes.main ];
           buildInputs =
             with pkgs;
             devPackages
