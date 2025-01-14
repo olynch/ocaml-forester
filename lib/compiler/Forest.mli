4a86f1d8f5e09edc7924fab1916f151e76225fd6
Kento Okura <kentookura@gmail.com>
2025-01-14 10:29:48 +0100

    Write some documentation for Forest.mli

--- lib/compiler/Forest.mli
+++ lib/compiler/Forest.mli
@@ -6,24 +6,33 @@
 
 open Forester_core
 
+include Hashtbl.S with type key = iri
+(**@closed*)
+
+(**/**)
 module T = Forester_core.Types
 module Dx = Forester_core.Datalog_expr
+val legacy_query_engine : (module Forest_graphs.S) -> (module Legacy_query_engine.S)
+
+val execute_datalog_script : (module Forest_graphs.S) -> (string, Vertex.t) Dx.sequent list -> unit
+(**/**)
+
+val analyse_resource : (module Forest_graphs.S) -> T.content T.resource -> unit
+(** [analyse_resource graphs resource] traverses {{!Forester_core.Types.resource}[resource]}, recording facts about it in {{!Forest_graphs.S}[graphs]}.
+
+    - When encountering a {{!Forester_core.Types.Link}[Link]}, it adds an edge to the {{!Forester_core.Builtin_relation.links_to}[links_to]} relation in {{!Forest_graphs.S}[graphs]}.
+
+    - When encountering a {{!Forester_core.Types.Transclude}[Transclude]}, it adds an edge to the {{!Forester_core.Builtin_relation.transcludes}[transcludes]} relation in {{!Forest_graphs.S}[graphs]}.
+
+    - When encountering a {{!Forester_core.Types.Datalog_script}[Datalog_script script]}, it runs the script and records the results in {{!Forest_graphs.S}[graphs]}.
+    *)
 
 val iri_for_resource : 'a T.resource -> Forester_core__.Base.iri option
 
-type env = (module Forest_graphs.S)
-
-include Hashtbl.S with type key = iri
-
-val legacy_query_engine : env -> (module Legacy_query_engine.S)
-[@@@ocaml.deprecated "The legacy query engine is deprecated"]
-
-val analyse_resource : env -> T.content T.resource -> unit
+val get_article : key -> T.content T.resource t -> T.content T.article option
 
 val run_datalog_query :
   (module Forest_graphs.S) -> (string, Forester_core.Vertex.t) Dx.query -> Forester_core.Vertex_set.t
-
-val get_article : key -> T.content T.resource t -> T.content T.article option
 
 val plant_resource :
   T.content T.resource ->
@@ -32,8 +41,8 @@ val plant_resource :
   unit
 
 val get_expanded_title :
-  ?scope:key ->
-  ?flags:T.title_flags ->
+  ?scope: key ->
+  ?flags: T.title_flags ->
   T.content T.frontmatter ->
   T.content T.resource t ->
   T.content
@@ -42,12 +51,14 @@ val get_content_of_transclusion :
   T.content T.transclusion -> T.content T.resource t -> T.content option
 
 val get_title_or_content_of_vertex :
-  ?not_found:(key -> T.content option) ->
-  modifier:T.modifier ->
-  T.content T.vertex -> T.content T.resource t -> T.content option
+  ?not_found: (key -> T.content option) ->
+  modifier: T.modifier ->
+  T.content T.vertex ->
+  T.content T.resource t ->
+  T.content option
 
 val get_all_articles :
   'a T.resource t -> 'a T.article list
 
-val get_all_resources : 
+val get_all_resources :
   'a t -> 'a list
