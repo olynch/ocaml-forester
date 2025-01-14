53a30c2cab3bb4b4304d4c8c2dcb03c2a2f5e93f
Kento Okura <kentookura@gmail.com>
2025-01-14 10:52:13 +0100

    formatting

--- lib/prelude/Eio_util.mli
+++ lib/prelude/Eio_util.mli
@@ -3,31 +3,37 @@ open Eio
 (* val ( / ) : ([> Fs.dir_ty ] as 'a) Path.t -> string -> 'a Path.t *)
 
 val path_of_dir :
-  env:< fs : ([> Fs.dir_ty ] as 'a) Path.t; .. > ->
-  string -> 'a Path.t
+  env: < fs: ([> Fs.dir_ty] as 'a) Path.t; .. > ->
+  string ->
+  'a Path.t
 
 val paths_of_dirs :
-  env:< fs : ([> Fs.dir_ty ] as 'a) Path.t; .. > ->
-  string list -> 'a Path.t list
+  env: < fs: ([> Fs.dir_ty] as 'a) Path.t; .. > ->
+  string list ->
+  'a Path.t list
 
 val null_sink : unit -> Flow.sink_ty Resource.t
 
 val ensure_context_of_path :
-  perm:File.Unix_perm.t -> [> Fs.dir_ty ] Path.t -> unit
+  perm: File.Unix_perm.t -> [> Fs.dir_ty] Path.t -> unit
 
-val ensure_remove_file : [> Fs.dir_ty ] Path.t -> unit
+val ensure_remove_file : [> Fs.dir_ty] Path.t -> unit
 
 val with_open_tmp_dir :
-  env:< cwd : [> Fs.dir_ty ] Path.t; .. > ->
-  ([< `Close | `Dir > `Dir ] Path.t -> 'a) -> 'a
+  env: < cwd: [> Fs.dir_ty] Path.t; .. > ->
+  ([< `Close | `Dir>`Dir] Path.t -> 'a) ->
+  'a
 
 val run_process :
-  ?quiet:bool ->
-  env:< process_mgr : [> [> `Generic ] Process.mgr_ty ] Process.mgr;
-        .. > ->
-  cwd:[> Fs.dir_ty ] Path.t -> string list -> unit
+  ?quiet: bool ->
+  env:
+  < process_mgr: [> [> `Generic] Process.mgr_ty] Process.mgr;
+  .. > ->
+  cwd: [> Fs.dir_ty] Path.t ->
+  string list ->
+  unit
 
-val file_exists : [> Fs.dir_ty ] Path.t -> bool
+val file_exists : [> Fs.dir_ty] Path.t -> bool
 
 (* val try_create_dir : cwd:[> Fs.dir_ty ] Path.t -> string -> unit *)
 (**)
@@ -35,8 +41,10 @@ val file_exists : [> Fs.dir_ty ] Path.t -> bool
 (*   cwd:[> Fs.dir_ty ] Path.t -> ?content:string -> string -> unit *)
 
 val copy_to_dir :
-  env:< process_mgr : [> [> `Generic ] Process.mgr_ty ] Process.mgr;
-        .. > ->
-  cwd:[> Fs.dir_ty ] Path.t ->
-  source:string -> dest_dir:string -> unit
-
+  env:
+  < process_mgr: [> [> `Generic] Process.mgr_ty] Process.mgr;
+  .. > ->
+  cwd: [> Fs.dir_ty] Path.t ->
+  source: string ->
+  dest_dir: string ->
+  unit
