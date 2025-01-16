148976028e537f36618b4d115f3c5c639466afaa
Kento Okura <kentookura@gmail.com>
2025-01-16 16:23:55 +0100

    More comprehensive testing of config parser

--- test/Test_config.ml
+++ test/Test_config.ml
@@ -5,6 +5,7 @@
  *)
 
 open Testables
+open Forester_core
 open Forester_compiler
 open Forester_frontend
 
@@ -17,7 +18,7 @@ let test_parsing () =
       home = Some "index";
       prefixes = ["foo"; "bar"; "baz"];
       assets = [];
-      foreign = [];
+      foreign = ["foreign/forest.json"];
       theme = "theme";
     }
     (
@@ -30,6 +31,163 @@ let test_parsing () =
         trees = ["trees"]
         home = "index"
         prefixes = ["foo", "bar", "baz"]
+        foreign = ["foreign/forest.json"]
+        |}
+    )
+
+let test_missing_fields () =
+  Alcotest.(check config)
+    "is the same"
+    Config.{
+      host = "test";
+      trees = ["trees"];
+      home = Some "index";
+      theme = "theme";
+      assets = [];
+      foreign = [];
+      prefixes = [];
+    }
+    (
+      Forester_core.Reporter.easy_run @@
+        fun () ->
+          Config_parser.parse_forest_config_string
+            {|
+        [forest]
+        host = "test"
+        trees = ["trees"]
+        home = "index"
+        |}
+    )
+
+let test_missing_host () =
+  Alcotest.(check unit)
+    "is the same"
+    ()
+    (
+      Forester_core.Reporter.run
+        ~fatal: (
+          function
+          | { message; explanation; _ } ->
+            Alcotest.(check Testables.message)
+              ""
+              Configuration_error
+              message;
+            Alcotest.(check string)
+              ""
+              (Asai.Diagnostic.string_of_text explanation.value)
+              "You need to set the `host' key in your configuration file; this is a global identifier that will be used to distinguish your forest from other forests (you can use your name, e.g. `johnqpublic')";
+        )
+        ~emit: (
+          function
+          | { message; explanation; _ } ->
+            Alcotest.(check Testables.message)
+              ""
+              Configuration_error
+              message
+        ) @@
+        fun () ->
+          let _ =
+            Config_parser.parse_forest_config_string
+              {|
+        [forest]
+        trees = ["trees"]
+        home = "index"
+        |}
+          in
+          assert false
+    )
+
+let test_parse_error () =
+  Alcotest.(check unit)
+    "is the same"
+    ()
+    (
+      Forester_core.Reporter.run
+        ~fatal: (
+          function
+          | { explanation; _ } ->
+            Alcotest.(check string)
+              ""
+              (Asai.Diagnostic.string_of_text explanation.value)
+              "Error in <anonymous> at line 2 at column 11 (position 12)";
+        )
+        ~emit: (fun _ -> ()) @@
+        fun () ->
+          let _ =
+            Config_parser.parse_forest_config_string
+              {|
+          ]]]
+        |}
+          in
+          assert false
+    )
+
+let test_stylesheet_warning () =
+  Alcotest.(check config)
+    "is the same"
+    Config.{
+      host = "test";
+      trees = ["trees"];
+      home = Some "index";
+      theme = "theme";
+      assets = [];
+      foreign = [];
+      prefixes = [];
+    }
+    (
+      Forester_core.Reporter.run
+        ~fatal: (fun _ -> assert false)
+        ~emit: (
+          function
+          | { explanation; _ } ->
+            (
+              Alcotest.(check string)
+                ""
+                "Custom XSL stylesheet injection is no longer supported; please remove the `stylesheet' key from the [forest] group."
+                (Asai.Diagnostic.string_of_text explanation.value)
+            )
+        ) @@
+        fun () ->
+          Config_parser.parse_forest_config_string
+            {|[forest]
+        host = "test"
+        trees = ["trees"]
+        home = "index"
+        stylesheet = "custom.xsl"
+        |}
+    )
+
+let test_root_warning () =
+  Alcotest.(check config)
+    "is the same"
+    Config.{
+      host = "test";
+      trees = ["trees"];
+      home = None;
+      theme = "theme";
+      assets = [];
+      foreign = [];
+      prefixes = [];
+    }
+    (
+      Forester_core.Reporter.run
+        ~fatal: (fun _ -> assert false)
+        ~emit: (
+          function
+          | { explanation; _ } ->
+            (
+              Alcotest.(check string)
+                ""
+                "In your configuration file, change `root' key to `home' in the [forest] group."
+                (Asai.Diagnostic.string_of_text explanation.value)
+            )
+        ) @@
+        fun () ->
+          Config_parser.parse_forest_config_string
+            {|[forest]
+        host = "test"
+        trees = ["trees"]
+        root = "index"
         |}
     )
 
@@ -39,5 +197,18 @@ let () =
     "Config parsing"
     [
       "example config works",
-      [test_case "" `Quick test_parsing];
+      [
+        test_case "it parses correctly" `Quick test_parsing;
+      ];
+      "can parse config with missing fields",
+      [
+        test_case "" `Quick test_missing_fields;
+      ];
+      "handles errors correctly",
+      [
+        test_case "" `Quick test_missing_host;
+        test_case "" `Quick test_parse_error;
+        test_case "" `Quick test_stylesheet_warning;
+        test_case "" `Quick test_root_warning;
+      ];
     ]
