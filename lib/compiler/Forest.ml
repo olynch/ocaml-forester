6a1ee0f98635dcd6c456afb40c25478d19742c55
Jon Sterling <jon@jonmsterling.com>
2025-02-18 22:42:59 +0000

    optimising Tbl in Forest
    This improves performance by almost double speed. I expose a stripped
    down interface that guarantees anything put into the forest has a
    normalized IRI.

--- lib/compiler/Forest.ml
+++ lib/compiler/Forest.ml
@@ -10,9 +10,38 @@ open Forester_core
 module T = Types
 module Q = Query
 
-module Tbl = Hashtbl.Make(Iri_hash)
+(* Ought to use Iri_hash, but I am optimising by avoiding noramlizing on equality comparison. *)
+module Tbl = Hashtbl.Make(struct
+  type t = iri
+  let equal = Iri.equal ~normalize: false
 
-include Tbl
+  (* IRI has mutable state that seems to be interfering with Hashtbl.hash *)
+  let clean iri = Iri.with_query iri (Iri.query iri)
+
+  (* If I remove the normalization here, everything fails. But I can't figure out why! *)
+  let hash iri = clean iri |> Hashtbl.hash
+end)
+
+type 'a t = 'a Tbl.t
+type key = iri
+
+let find_opt tbl iri =
+  Tbl.find_opt tbl (Iri.normalize iri)
+
+let mem tbl iri =
+  Tbl.mem tbl (Iri.normalize iri)
+
+let add tbl iri x =
+  Tbl.add tbl (Iri.normalize iri) x
+
+let replace tbl iri x =
+  Tbl.replace tbl (Iri.normalize iri) x
+
+let length = Tbl.length
+let create = Tbl.create
+let iter = Tbl.iter
+let to_seq_values = Tbl.to_seq_values
+let to_seq_keys = Tbl.to_seq_keys
 
 type resource = T.content T.resource
 
--- lib/compiler/Forest.mli
+++ lib/compiler/Forest.mli
@@ -6,8 +6,17 @@
 
 open Forester_core
 
-include Hashtbl.S with type key = iri
-(**@closed*)
+type 'a t
+type key = iri
+
+val create : int -> 'a t
+val find_opt : 'a t -> key -> 'a option
+val add : 'a t -> key -> 'a -> unit
+val length : 'a t -> int
+val to_seq_values : 'a t -> 'a Seq.t
+val to_seq_keys : 'a t -> key Seq.t
+val replace : 'a t -> key -> 'a -> unit
+val iter : (key -> 'a -> unit) -> 'a t -> unit
 
 (**/**)
 module T = Forester_core.Types
