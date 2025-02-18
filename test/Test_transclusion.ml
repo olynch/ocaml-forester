9e0219a54758de858fb50ba88aa856ce1fdbf76d
Kento Okura <kentookura@gmail.com>
2025-02-18 11:47:58 +0100

    Refactor transclusion test

--- test/Test_transclusion.ml
+++ test/Test_transclusion.ml
@@ -41,67 +41,51 @@ end
 
 let () =
   let@ env = Eio_main.run in
-  let open Alcotest in
   let@ () = Reporter.easy_run in
   (* Needs to be false to make tests reproducible. The source path depends on the host *)
   let tree_dirs = Eio_util.paths_of_dirs ~env config.trees in
   let forest =
     Phases.init ~env ~config ~dev: false
-    |> State_machine.(run_action Load_all ~until: Do_nothing)
+    |> State_machine.(run_action Load_all_configured_dirs)
   in
   let iri = Iri_scheme.user_iri ~host "transcludee" in
-  let test_transclusion t expect =
-    let result = Forest.get_content_of_transclusion t forest.resources in
-    Alcotest.(check @@ option string)
-      ""
-      expect
-      (
-        Option.map
-          (
-            fun content ->
-              Format.asprintf
-                "%a"
-                Render.(pp_xml ~forest ~dev: false ?stylesheet: None)
-                T.{
-                  frontmatter = default_frontmatter ~iri: href ();
-                  mainmatter = content;
-                  backmatter = Content []
-                }
-          )
-          result
-      )
+  let print_transclusion
+      : T.content T.transclusion -> unit
+    = fun t ->
+      let content = Option.get @@ Forest.get_content_of_transclusion t forest.resources in
+      Format.printf
+        "%a"
+        Render.(pp_xml ~forest ~dev: false ?stylesheet: None)
+        T.{
+          frontmatter = default_frontmatter ~iri: href ();
+          mainmatter = content;
+          backmatter = Content []
+        }
   in
   let test_full_default () =
-    test_transclusion
+    print_transclusion
       Transclusions.full_default
-      (
-        Some
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n<fr:tree xmlns:fr=\"http://www.jonmsterling.com/jms-005P.xml\" root=\"false\">\n  <fr:frontmatter>\n    <fr:authors />\n    <fr:addr>transcludee</fr:addr>\n    <fr:route>transcludee.xml</fr:route>\n    <fr:title text=\"\" />\n  </fr:frontmatter>\n  <fr:mainmatter>\n    <fr:tree show-metadata=\"false\">\n      <fr:frontmatter>\n        <fr:authors />\n        <fr:addr>transcludee</fr:addr>\n        <fr:route>transcludee.xml</fr:route>\n        <fr:title text=\"I am being transcluded\">I am being transcluded</fr:title>\n      </fr:frontmatter>\n      <fr:mainmatter />\n    </fr:tree>\n  </fr:mainmatter>\n  <fr:backmatter />\n</fr:tree>\n"
-      )
   in
   let test_title_default () =
-    test_transclusion
-      { href = iri; target = Title { empty_when_untitled = false }; modifier = Identity }
-      (
-        Some
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n<fr:tree xmlns:fr=\"http://www.jonmsterling.com/jms-005P.xml\" root=\"false\">\n  <fr:frontmatter>\n    <fr:authors />\n    <fr:addr>transcludee</fr:addr>\n    <fr:route>transcludee.xml</fr:route>\n    <fr:title text=\"\" />\n  </fr:frontmatter>\n  <fr:mainmatter>I am being transcluded</fr:mainmatter>\n  <fr:backmatter />\n</fr:tree>\n"
-      )
+    print_transclusion
+      {
+        href = iri;
+        target = Title { empty_when_untitled = false };
+        modifier = Identity
+      }
   in
   let test_full_metadata () =
-    test_transclusion
-      { href = iri; target = Full Transclusions.metadata_shown; modifier = Identity }
-      (
-        Some
-          "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n<fr:tree xmlns:fr=\"http://www.jonmsterling.com/jms-005P.xml\" root=\"false\">\n  <fr:frontmatter>\n    <fr:authors />\n    <fr:addr>transcludee</fr:addr>\n    <fr:route>transcludee.xml</fr:route>\n    <fr:title text=\"\" />\n  </fr:frontmatter>\n  <fr:mainmatter>\n    <fr:tree show-metadata=\"true\">\n      <fr:frontmatter>\n        <fr:authors />\n        <fr:addr>transcludee</fr:addr>\n        <fr:route>transcludee.xml</fr:route>\n        <fr:title text=\"I am being transcluded\">I am being transcluded</fr:title>\n      </fr:frontmatter>\n      <fr:mainmatter />\n    </fr:tree>\n  </fr:mainmatter>\n  <fr:backmatter />\n</fr:tree>\n"
-      )
+    print_transclusion
+      {
+        href = iri;
+        target = Full Transclusions.metadata_shown;
+        modifier = Identity
+      }
   in
-  run
-    "Transclusion configuration"
+  List.iter
+    (fun f -> f ())
     [
-      "works",
-      [
-        test_case "check full transclusion with default section flags" `Quick test_full_default;
-        test_case "check title transclusion with default title flags" `Quick test_title_default;
-        test_case "check full transclusion with metadata shown" `Quick test_full_metadata;
-      ]
+      test_full_default;
+      test_title_default;
+      test_full_metadata
     ]
---
+++ test/Transclude.t
@@ -0,0 +1,57 @@
+  $ transclude
+  <?xml version="1.0" encoding="UTF-8"?>
+  
+  <fr:tree xmlns:fr="http://www.jonmsterling.com/jms-005P.xml" root="false">
+    <fr:frontmatter>
+      <fr:authors />
+      <fr:addr>transcludee</fr:addr>
+      <fr:route>transcludee.xml</fr:route>
+      <fr:title text="" />
+    </fr:frontmatter>
+    <fr:mainmatter>
+      <fr:tree show-metadata="false">
+        <fr:frontmatter>
+          <fr:authors />
+          <fr:addr>transcludee</fr:addr>
+          <fr:route>transcludee.xml</fr:route>
+          <fr:title text="I am being transcluded">I am being transcluded</fr:title>
+        </fr:frontmatter>
+        <fr:mainmatter />
+      </fr:tree>
+    </fr:mainmatter>
+    <fr:backmatter />
+  </fr:tree>
+  <?xml version="1.0" encoding="UTF-8"?>
+  
+  <fr:tree xmlns:fr="http://www.jonmsterling.com/jms-005P.xml" root="false">
+    <fr:frontmatter>
+      <fr:authors />
+      <fr:addr>transcludee</fr:addr>
+      <fr:route>transcludee.xml</fr:route>
+      <fr:title text="" />
+    </fr:frontmatter>
+    <fr:mainmatter>I am being transcluded</fr:mainmatter>
+    <fr:backmatter />
+  </fr:tree>
+  <?xml version="1.0" encoding="UTF-8"?>
+  
+  <fr:tree xmlns:fr="http://www.jonmsterling.com/jms-005P.xml" root="false">
+    <fr:frontmatter>
+      <fr:authors />
+      <fr:addr>transcludee</fr:addr>
+      <fr:route>transcludee.xml</fr:route>
+      <fr:title text="" />
+    </fr:frontmatter>
+    <fr:mainmatter>
+      <fr:tree show-metadata="true">
+        <fr:frontmatter>
+          <fr:authors />
+          <fr:addr>transcludee</fr:addr>
+          <fr:route>transcludee.xml</fr:route>
+          <fr:title text="I am being transcluded">I am being transcluded</fr:title>
+        </fr:frontmatter>
+        <fr:mainmatter />
+      </fr:tree>
+    </fr:mainmatter>
+    <fr:backmatter />
+  </fr:tree>
