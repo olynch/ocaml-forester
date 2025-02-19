df9c87df69a37bd9ea7f63362c0132283799fd67
Jon Sterling <jon@jonmsterling.com>
2025-02-19 14:21:54 +0000

    move Iri_tbl into Base

--- lib/core/Base.ml
+++ lib/core/Base.ml
@@ -21,6 +21,41 @@ module Iri_set = Set.Make(Iri_ord)
 module Iri_map = Map.Make(Iri_ord)
 module String_map = Map.Make(String)
 
+module Iri_tbl = struct
+
+  (* Ought to use Iri_hash, but I am optimising by avoiding normalizing on equality comparison. *)
+  module Tbl = Hashtbl.Make(struct
+    type t = iri
+    let equal = Iri.equal ~normalize: false
+
+    (* IRI has mutable state that seems to be interfering with Hashtbl.hash *)
+    let clean iri = Iri.with_query iri (Iri.query iri)
+
+    let hash iri = clean iri |> Hashtbl.hash
+  end)
+
+  type 'a t = 'a Tbl.t
+  type key = iri
+
+  let find_opt tbl iri =
+    Tbl.find_opt tbl (Iri.normalize iri)
+
+  let mem tbl iri =
+    Tbl.mem tbl (Iri.normalize iri)
+
+  let add tbl iri x =
+    Tbl.add tbl (Iri.normalize iri) x
+
+  let replace tbl iri x =
+    Tbl.replace tbl (Iri.normalize iri) x
+
+  let length = Tbl.length
+  let create = Tbl.create
+  let iter = Tbl.iter
+  let to_seq_values = Tbl.to_seq_values
+  let to_seq_keys = Tbl.to_seq_keys
+end
+
 type delim =
   Braces | Squares | Parens
 [@@deriving show, repr]
--- lib/core/Base.mli
+++ lib/core/Base.mli
@@ -31,6 +31,21 @@ module Iri_map: sig
   val to_list : 'a t -> (key * 'a) list
 end
 
+module Iri_tbl: sig
+  type 'a t
+  type key = iri
+
+  val create : int -> 'a t
+  val mem : 'a t -> key -> bool
+  val find_opt : 'a t -> key -> 'a option
+  val add : 'a t -> key -> 'a -> unit
+  val length : 'a t -> int
+  val to_seq_values : 'a t -> 'a Seq.t
+  val to_seq_keys : 'a t -> key Seq.t
+  val replace : 'a t -> key -> 'a -> unit
+  val iter : (key -> 'a -> unit) -> 'a t -> unit
+end
+
 (** {2 Delimiters} *)
 
 type delim = Braces | Squares | Parens
