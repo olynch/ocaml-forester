b0ae73f6015b6160affcfecabb014cda91ee5a31
Jon Sterling <jon@jonmsterling.com>
2025-02-20 09:36:16 +0000

    replace Iri_hash_unsafe with Iri_hash_safe

--- lib/core/Base.ml
+++ lib/core/Base.ml
@@ -14,42 +14,48 @@ let pp_iri (fmt : Format.formatter) (iri : Iri.t) =
 
 module String_map = Map.Make(String)
 
-module Iri_hash_unsafe = struct
+module Iri_hash_safe : sig
+  type t = private iri
+  val make : iri -> t
+  include Hashtbl.HashedType with type t := t
+end = struct
   type t = iri
+  let make (x : iri) : t = Iri.normalize x
   let equal = Iri.equal ~normalize: false
 
   let distill iri = Iri.scheme iri, Iri.host iri, Iri.port iri, Iri.user iri, Iri.path iri
 
   (* IRI has mutable state that seems to be interfering with Hashtbl.hash *)
-  let hash iri = Hashtbl.hash (distill iri)
+  let hash (iri : t) = Hashtbl.hash (distill iri)
 end
 
 module Iri_set = Iri.Set
 module Iri_map = Iri.Map
 
 module Iri_tbl = struct
-  module Tbl = Hashtbl.Make(Iri_hash_unsafe)
+  module S = Iri_hash_safe
+  module Tbl = Hashtbl.Make(S)
 
   type 'a t = 'a Tbl.t
   type key = iri
 
   let find_opt tbl iri =
-    Tbl.find_opt tbl (Iri.normalize iri)
+    Tbl.find_opt tbl (S.make iri)
 
   let mem tbl iri =
-    Tbl.mem tbl (Iri.normalize iri)
+    Tbl.mem tbl (S.make iri)
 
   let add tbl iri x =
-    Tbl.add tbl (Iri.normalize iri) x
+    Tbl.add tbl (S.make iri) x
 
   let replace tbl iri x =
-    Tbl.replace tbl (Iri.normalize iri) x
+    Tbl.replace tbl (S.make iri) x
 
   let length = Tbl.length
   let create = Tbl.create
-  let iter = Tbl.iter
+  let iter f = Tbl.iter (fun x y -> f (x : S.t :> iri) y)
   let to_seq_values = Tbl.to_seq_values
-  let to_seq_keys = Tbl.to_seq_keys
+  let to_seq_keys tbl = Tbl.to_seq_keys tbl |> Seq.map (fun x -> (x : S.t :> iri))
 end
 
 type delim =
